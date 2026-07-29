e7910c45536532899e10d67cf7c321521d47ec81
mary07-ux <mary.rocha@escola.pr.gov.br>
2026-07-29 16:28:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+cost numerosenha = document.queryselector('.parametro-senha_texto');
+let tamanhoSnha=12;
+numeroSenha.texCtent= tamanhoSenha;
+
+cont botoes =document.querySelectorAll('.pareament-senha_botao');
+
+function diminuiTamanho() {
+    if(tamanhoSenha)
+}
