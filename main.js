61f087e62b3a55938a11cd5adaa33b0352d065ad
mary07-ux <mary.rocha@escola.pr.gov.br>
2026-08-05 11:39:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-cost numerosenha = document.queryselector('.parametro-senha_texto');
-let tamanhoSnha=12;
-numeroSenha.texCtent= tamanhoSenha;
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
-cont botoes =document.querySelectorAll('.pareament-senha_botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
 
 function diminuiTamanho() {
-    if(tamanhoSenha)
+    if (tamanhoSenha>1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+   
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
+
+function aumentaTamanho() {
+    if (tamanhoSenha<20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+   
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    for(let i=0; i<tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        console.log(letrasMaiusculas[numeroAleatorio]);
+    }
+}
+campoSenha.value = letrasMaiusculas;
